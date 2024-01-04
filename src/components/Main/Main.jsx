66aeb6f1285aa426a766995498f2645523eeb29d
Indo Krish <55687559+Indok93@users.noreply.github.com>
2024-01-04 10:13:46 +0100

    short styling

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,8 @@
 export const Main = () => {
     return (
         <main>
-            <article>
-                <h2>Heading numero dos</h2>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet quidem, excepturi asperiores tempore cum, dolorum deleniti, earum similique sequi itaque recusandae beatae sit natus est! Dolorem deserunt repudiandae quaerat quod.</p>
-            </article>
+            <h2>Heading numero dos</h2>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet quidem, excepturi asperiores tempore cum, dolorum deleniti, earum similique sequi itaque recusandae beatae sit natus est! Dolorem deserunt repudiandae quaerat quod.</p>
         </main>
     )
 }
