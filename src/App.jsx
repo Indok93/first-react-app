a4c9cea90b252852b3dff8e3afe3f7e208ff44f7
Indo Krish <55687559+Indok93@users.noreply.github.com>
2024-01-10 04:16:39 +0100

    contentwrapper added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import './App.scss'
 import { Header } from './components/Header/Header'
 import { Main } from './components/Main/Main'
 import { Footer } from './components/Footer/Footer'
+import { ContentWrapper } from './components/ContentWrapper/ContentWrapper'
 
 function App() {
   return (
     <>
     <Header />
+    <ContentWrapper title="Dette er en sidetitel">
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quasi repellendus error repudiandae temporibus itaque rerum molestias adipisci eum veniam asperiores eos deleniti cupiditate in corrupti, voluptatem nisi porro laudantium dolores.</p>
+    </ContentWrapper>
     <Main />
     <Footer />
     </>
